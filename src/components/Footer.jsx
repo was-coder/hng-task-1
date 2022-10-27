b4bcd78406c5f4323a87d10cd93f92a89c33016f
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-10-27 21:32:16 +0100

    feat:Finish the footer section and add images that are require

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,15 @@ import React, { Fragment } from "react";
 function Footer() {
   return (
     <Fragment>
-      <footer className="footer-container"></footer>
+      <footer className="footer-container">
+        <img
+          src="/assets/internship.png"
+          alt="zuri Internship"
+          className="footer-img"
+        />
+        <p id="task-paragraph">HNG Internship 9 Frontend Task</p>
+        <img src="/assets/I4G.png" alt="I4G logo" className="footer-img" />
+      </footer>
     </Fragment>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -14,7 +14,7 @@ function Profile() {
   return (
     <Fragment>
       <header className="profile-header">
-        <img src="asset/wasiu.jfif" alt="Wasiu's Profile" id="profile__img" />
+        <img src="assets/wasiu.jfif" alt="Wasiu's Profile" id="profile__img" />
         <h1 id="twitter" onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
           DevWaslead
         </h1>
@@ -22,14 +22,14 @@ function Profile() {
 
         <div className="icon-div" id="mobile-share">
           <img
-            src="asset/mobile-share.png"
+            src="assets/mobile-share.png"
             alt="share icon"
             className="share"
           />
         </div>
 
         <div className="icon-div" id="desktop-share">
-          <img src="asset/desktop-share.png" alt="desktop-share icon" />
+          <img src="assets/desktop-share.png" alt="desktop-share icon" />
         </div>
       </header>
     </Fragment>
