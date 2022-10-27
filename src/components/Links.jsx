a16b66a813dece2b94faf616818d76d0ae2ce3d5
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-10-27 22:37:36 +0100

    feat:Work on the links section by calling the data api

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,9 +1,54 @@
 import React, { Fragment } from "react";
 
 function Links() {
+  const data = [
+    {
+      id: "btn__twitter",
+      content: "Twitter Link",
+      link: "https://twitter.com/DevWaslead/",
+    },
+    {
+      id: "btn__zuri",
+      content: "Zuri Team",
+      link: "https://training.zuri.team/",
+    },
+    {
+      id: "books",
+      content: "Zuri Books",
+      link: "http://books.zuri.team/",
+    },
+    {
+      id: "book__python",
+      content: "Python Books",
+      link: "https://books.zuri.team/python-for-beginners?ref_id=waslead",
+    },
+    {
+      id: "pitch",
+      content: "Background Check for Coders",
+      link: "https://background.zuri.team/",
+    },
+    {
+      id: "book__design",
+      content: "Design Books",
+      link: "https://books.zuri.team/design-rules",
+    },
+  ];
+
   return (
     <Fragment>
-      <h1>Links</h1>
+      <section className="link-section">
+        {data.map((data) => (
+          <a
+            className="btn"
+            target="_blank"
+            rel="noopener noreferrer"
+            key={data.id}
+            href={data.link}
+          >
+            {data.content}
+          </a>
+        ))}
+      </section>
     </Fragment>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -14,7 +14,7 @@ function Profile() {
   return (
     <Fragment>
       <header className="profile-header">
-        <img src="assets/wasiu.jfif" alt="Wasiu's Profile" id="profile__img" />
+        <img src="/assets/wasiu.jfif" alt="Wasiu's Profile" id="profile__img" />
         <h1 id="twitter" onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
           DevWaslead
         </h1>
@@ -22,14 +22,18 @@ function Profile() {
 
         <div className="icon-div" id="mobile-share">
           <img
-            src="assets/mobile-share.png"
+            src="/assets/mobile-share.png"
             alt="share icon"
             className="share"
           />
         </div>
 
         <div className="icon-div" id="desktop-share">
-          <img src="assets/desktop-share.png" alt="desktop-share icon" />
+          <img
+            src="/assets/desktop-share.png"
+            alt="desktop-share icon"
+            className="share"
+          />
         </div>
       </header>
     </Fragment>
